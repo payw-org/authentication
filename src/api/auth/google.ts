faf15e93d5e9227fb07fcc057c9f869ae3b831eb
jhaemin <io@jhaemin.com>
2020-10-26 20:46:27 +0900

    refactor: modularize google auth router

--- src/api/auth/google.ts
+++ src/api/auth/google.ts
@@ -7,85 +7,95 @@ import express from 'express'
 import passport from 'passport'
 import * as GoogleStrategy from 'passport-google-oauth'
 
-const redirectURL = '/google/redirect'
+function makeGoogleRouter({ appName }: { appName: string }) {
+  const redirectURL = `/google/redirect/${appName}`
 
-passport.use(
-  'google',
-  new GoogleStrategy.OAuth2Strategy(
-    {
-      clientID: env.auth.google.clientID,
-      clientSecret: env.auth.google.clientSecret,
-      callbackURL: redirectURL,
-    },
-    async (...args) => {
-      const profile: GoogleStrategy.Profile = args[2]
-      const done: GoogleStrategy.VerifyFunction = args[3]
+  passport.use(
+    `google-${appName}`,
+    new GoogleStrategy.OAuth2Strategy(
+      {
+        clientID: env.auth.google.clientID,
+        clientSecret: env.auth.google.clientSecret,
+        callbackURL: redirectURL,
+      },
+      async (...args) => {
+        const profile: GoogleStrategy.Profile = args[2]
+        const done: GoogleStrategy.VerifyFunction = args[3]
 
-      if (!profile.emails) {
-        done(Error(`Email doesn't exist`))
-        return
-      }
+        if (!profile.emails) {
+          done(Error(`Email doesn't exist`))
+          return
+        }
 
-      const existingUser = await prisma.user.findOne({
-        where: {
-          userID: profile.emails[0].value,
-        },
-      })
-
-      if (existingUser) {
-        done(null, {
-          userID: existingUser.id,
+        const existingUser = await prisma.user.findOne({
+          where: {
+            userID: profile.emails[0].value,
+          },
         })
-        return
+
+        if (existingUser) {
+          done(null, {
+            userID: existingUser.id,
+          })
+          return
+        }
+
+        const createdUser = await prisma.user.create({
+          data: {
+            userID: profile.emails[0].value,
+            registeredAt: currentTime(),
+          },
+        })
+
+        const authData: AuthData = {
+          userID: createdUser.id,
+        }
+
+        done(null, authData)
       }
-
-      const createdUser = await prisma.user.create({
-        data: {
-          userID: profile.emails[0].value,
-          registeredAt: currentTime(),
-        },
-      })
-
-      const authData: AuthData = {
-        userID: createdUser.id,
-      }
-
-      done(null, authData)
-    }
+    )
   )
-)
 
-const googleAuthRouter = express.Router()
+  const googleAuthRouter = express.Router()
 
-googleAuthRouter.get(
-  '/google/sign-up',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-    session: false,
-  })
-)
-
-googleAuthRouter.get(redirectURL, (req, res, next) => {
-  passport.authenticate(
-    'google',
-    {
+  googleAuthRouter.get(
+    `/google/sign-up/${appName}`,
+    passport.authenticate(`google-${appName}`, {
+      scope: ['profile', 'email'],
       session: false,
-    },
-    async (err, authData: AuthData) => {
-      const accessToken = signAccessToken(authData)
-      const refreshToken = await signRefreshToken(authData)
+    })
+  )
 
-      res.cookie('accessToken', accessToken, {
-        path: '/',
-        httpOnly: true,
-      })
-      res.cookie('refreshToken', refreshToken, {
-        path: '/',
-        httpOnly: true,
-      })
-      res.redirect('/')
-    }
-  )(req, res, next)
+  googleAuthRouter.get(redirectURL, (req, res, next) => {
+    passport.authenticate(
+      `google-${appName}`,
+      {
+        session: false,
+      },
+      async (err, authData: AuthData) => {
+        console.log(authData)
+
+        const accessToken = signAccessToken(authData)
+        const refreshToken = await signRefreshToken(authData)
+
+        res.cookie('accessToken', accessToken, {
+          path: '/',
+          httpOnly: true,
+        })
+        res.cookie('refreshToken', refreshToken, {
+          path: '/',
+          httpOnly: true,
+        })
+        res.redirect('/')
+      }
+    )(req, res, next)
+  })
+
+  return googleAuthRouter
+}
+
+const googleAuthRouter = makeGoogleRouter({
+  appName: 'saying',
 })
 
 export { googleAuthRouter }
