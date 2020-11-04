69a0bc9d82cc0c59718c452c5bb96fd5f5f46c65
jhaemin <io@jhaemin.com>
2020-11-04 20:58:41 +0900

    feat(types): type environment variables
    * NODE_ENV: 'production' or 'development'
    * HOST: 'local' or 'origin'

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,12 @@
 export type AuthData = {
   userID: number
 }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production'
+      HOST: 'local' | 'remote'
+    }
+  }
+}
