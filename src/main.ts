5c84f5e842c7aee85300e65822201a8f0b3b88ee
jhaemin <io@jhaemin.com>
2020-11-05 22:49:19 +0900

    fix: clear server instance before restart

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,22 @@ import bodyParser from 'body-parser'
 import chalk from 'chalk'
 import express from 'express'
 import { apiRouter } from './api'
+import { prisma } from './modules/prisma'
 
 const dev = process.env.NODE_ENV === 'development'
 
-const server = express()
+const app = express()
 
-server.use(bodyParser.json())
-server.use(apiRouter)
+app.use(bodyParser.json())
+app.use(apiRouter)
 
-// const port = dev ? 4000 : 15100
 const port = dev ? env.port.dev : env.port.prod
 
-server.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`${chalk.yellow('server')} - listening on port ${port}`)
+})
+
+prisma.$on('beforeExit' as never, () => {
+  prisma.$disconnect()
+  server.close()
 })
