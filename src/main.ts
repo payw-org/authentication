2f56e544f8a2e0b488ea5e9aee50ff1f518c18bb
jhaemin <io@jhaemin.com>
2020-11-07 21:36:17 +0900

    feat: create an entry page

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { env } from '@/env'
 import bodyParser from 'body-parser'
 import chalk from 'chalk'
 import express from 'express'
+import path from 'path'
 import { apiRouter } from './api'
 import { prisma } from './modules/prisma'
 
@@ -10,6 +11,7 @@ const dev = process.env.NODE_ENV === 'development'
 const app = express()
 
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(apiRouter)
 
 const port = dev ? env.port.dev : env.port.prod
