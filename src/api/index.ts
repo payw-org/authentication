2e1cb1f8419699bcfd350e2bd7da734d2055a5aa
jhaemin <io@jhaemin.com>
2020-11-17 21:36:08 +0900

    feat: use verification router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { authRouter } from './auth'
+import { verificationRouter } from './verification'
 
 const apiRouter = express.Router()
 
 apiRouter.use(authRouter)
+apiRouter.use(verificationRouter)
 
 export { apiRouter }
