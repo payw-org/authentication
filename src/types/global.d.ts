84acc2e8a580b3e7bec8284c87146984dcc6be27
jhaemin <io@jhaemin.com>
2020-11-19 23:44:14 +0900

    feat(types): declare express res.locals globally

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,18 @@
+import { AuthData } from '@/modules/auth'
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production'
       HOST: 'local' | 'remote'
+    }
+  }
+}
+
+declare module 'express' {
+  export interface Response {
+    locals: {
+      authData: AuthData
     }
   }
 }
