577cf958c8e9d16e747568fdf3afc91a802cd29f
jhaemin <io@jhaemin.com>
2020-11-20 19:40:51 +0900

    refactor: use examineToken middleware

--- src/api/verification/index.ts
+++ src/api/verification/index.ts
@@ -1,3 +1,4 @@
+import { examineToken } from '@/middleware'
 import { verifyToken } from '@/modules/auth'
 import { makeBody, makeHeader, validate } from '@/modules/express-validator'
 import type { Request, Response } from 'express'
@@ -8,88 +9,72 @@ const header = makeHeader<{ authorization: string }>()
 
 const verificationRouter = express.Router()
 
+verificationRouter.use(examineToken)
+
 type VerificationRequestHeaderAuthorization = {
   accessToken: string
   refreshToken: string
 }
 
 // TODO: validate from headers
-verificationRouter.post(
-  `/verify`,
-  validate([header('authorization').isString()]),
-  async (req: Request, res: Response) => {
-    const authorization = req.headers.authorization
+verificationRouter.post(`/verify`, async (req: Request, res: Response) => {
+  const accessToken = res.locals.token as string
 
-    if (!authorization || typeof authorization !== 'string') {
-      res.sendStatus(401)
-      return
-    }
+  const [accessTokenErr, authDataFromAccessToken] = await verifyToken(
+    accessToken,
+    'access'
+  )
 
-    const splittedAuth = authorization.split(' ')
+  console.log(accessTokenErr)
 
-    const bearerPart = splittedAuth[0]
-    const accessToken = splittedAuth[1]
+  console.log(authDataFromAccessToken)
 
-    if (bearerPart !== 'Bearer' || !accessToken) {
-      res.sendStatus(401)
-      return
-    }
-
-    const [accessTokenErr, authDataFromAccessToken] = await verifyToken(
-      accessToken
-    )
-
-    console.log(accessTokenErr)
-
-    console.log(authDataFromAccessToken)
-
-    if (accessTokenErr === null) {
-      res.json(authDataFromAccessToken)
-      return
-    }
-
-    if (accessTokenErr.name !== 'TokenExpiredError') {
-      res.sendStatus(401)
-      return
-    }
-
-    // Access token expired
-
-    res.status(401).send({ shouldRefresh: true })
-
-    // const [refreshTokenErr, authDataFromRefreshToken] = await verifyToken(
-    //   refreshToken
-    // )
-
-    // if (refreshTokenErr || !authDataFromRefreshToken) {
-    //   res.sendStatus(401)
-    //   return
-    // }
-
-    // const foundUser = await prisma.user.findOne({
-    //   where: {
-    //     id: authDataFromRefreshToken.userID,
-    //   },
-    // })
-
-    // if (!foundUser || foundUser.refreshToken !== refreshToken) {
-    //   res.sendStatus(401)
-    //   return
-    // }
-
-    // console.log(authDataFromRefreshToken)
-
-    // const newAccessToken = signAccessToken({
-    //   userID: authDataFromRefreshToken.userID,
-    // })
-
-    // console.log(newAccessToken)
-
-    // res.json({
-    //   accessToken: newAccessToken,
-    // })
+  if (accessTokenErr === null) {
+    res.json(authDataFromAccessToken)
+    return
   }
-)
+
+  if (accessTokenErr.name !== 'TokenExpiredError') {
+    res.sendStatus(401)
+    return
+  }
+
+  // Access token expired
+
+  res.status(401).send({ shouldRefresh: true })
+
+  // const [refreshTokenErr, authDataFromRefreshToken] = await verifyToken(
+  //   refreshToken
+  // )
+
+  // if (refreshTokenErr || !authDataFromRefreshToken) {
+  //   res.sendStatus(401)
+  //   return
+  // }
+
+  // const foundUser = await prisma.user.findOne({
+  //   where: {
+  //     id: authDataFromRefreshToken.userID,
+  //   },
+  // })
+
+  // if (!foundUser || foundUser.refreshToken !== refreshToken) {
+  //   res.sendStatus(401)
+  //   return
+  // }
+
+  // console.log(authDataFromRefreshToken)
+
+  // const newAccessToken = signAccessToken({
+  //   userID: authDataFromRefreshToken.userID,
+  // })
+
+  // console.log(newAccessToken)
+
+  // res.json({
+  //   accessToken: newAccessToken,
+  // })
+})
 
 verificationRouter.post(
   `/refresh`,
