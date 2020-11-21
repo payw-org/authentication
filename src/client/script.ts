57b4e7b63b849070a200104ff9da2f72995d332d
jhaemin <io@jhaemin.com>
2020-11-21 21:55:06 +0900

    feat: handle service availability

--- src/client/script.ts
+++ src/client/script.ts
@@ -1,5 +1,6 @@
 import { gsap, Power3 } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import queryString from 'query-string'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -52,10 +53,42 @@ tl.fromTo(
   }
 )
 
-tl.to('.google', {
+tl.to('.connected-service', {
   opacity: 1,
   duration: 1,
   delay: -0.7,
 })
 
-console.log(window.location.search)
+tl.to('.google', {
+  opacity: 1,
+  duration: 1,
+  delay: -0.8,
+})
+
+const availableServices = ['saying.today', 'where.land']
+
+function verifyService(serviceName: string) {
+  return availableServices.includes(serviceName)
+}
+
+const query = queryString.parseUrl(window.location.href).query as {
+  service?: string
+}
+
+const available = verifyService(query.service)
+
+const connectedServiceElm = document.querySelector('.connected-service')
+const googleBtn = document.querySelector<HTMLAnchorElement>('.google')
+
+if (!available) {
+  connectedServiceElm.innerHTML = `각 서비스에서 로그인을 요청하세요.`
+  googleBtn.remove()
+} else {
+  connectedServiceElm.innerHTML = `서비스: ${query.service}`
+
+  const dev = process.env.NODE_ENV === 'development'
+
+  const host = dev ? 'http://localhost:3020' : ''
+
+  googleBtn.href = `${host}/google/sign-up/${query.service}`
+}
