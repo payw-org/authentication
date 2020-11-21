d9210dc81c82bd8c3aba93e2c8723bfa41443a29
jhaemin <io@jhaemin.com>
2020-11-21 22:47:52 +0900

    refactor(types): remove extended express locals type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,19 +1,8 @@
-import { AuthData } from '@/modules/auth'
-
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production'
       HOST: 'local' | 'remote'
-    }
-  }
-}
-
-declare module 'express' {
-  export interface Response {
-    locals: {
-      token?: string
-      authData?: AuthData
     }
   }
 }
