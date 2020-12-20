e5b82da4d2293817dcea14297cc9344112a71493
Jang Haemin <io@jhaemin.com>
2020-12-20 14:31:26 +0900

    fix(types): handler returns nothing

--- src/modules/express-helper.ts
+++ src/modules/express-helper.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from 'express'
 
 export const createHandler = (
-  handler: (ctx: { req: Request; res: Response; next: NextFunction }) => any
+  handler: (ctx: { req: Request; res: Response; next: NextFunction }) => void
 ) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     handler({ req, res, next })
