2b51af59c7df6f6f916e5a2107700624e1789285
Jang Haemin <io@jhaemin.com>
2021-05-21 19:29:35 +0900

    Extend express request interface

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,16 @@
+import 'express-serve-static-core'
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production'
       HOST: 'local' | 'remote'
     }
+  }
+}
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    token: string
   }
 }
